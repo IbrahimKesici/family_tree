dcb2eef6c674dc8dddadd00c3a5aa83737299834
kesici <ibrahim.kesici@outlook.com>
2022-03-16 20:29:36 +0100

    Created member class

--- family_tree/member.py
+++ family_tree/member.py
@@ -10,16 +10,33 @@ class Gender(Enum):
 @dataclass(repr=True)
 class Member():
     
-    id: int
     name: str
     gender: Gender
     father: object
     mother: object
     spouse: object = None
     children: List[object] = field(default_factory=list)
+    _id: int = 0
+    
+    @property
+    def id(self) -> int:
+        return self._id 
+       
+    def __post_init__(self) -> None:
+        self._id += 1
+        if self.father is not None and self.father.gender != Gender.MALE:
+            raise ValueError(f'Invalid gender: {self.father.gender.name} for father!')
+        
+        if self.mother is not None and self.mother.gender != Gender.FEMALE:
+            raise ValueError(f'Invalid gender: {self.mother.gender.name} for mother!')
+            
+    def set_spouse(self, spouse: object) -> None:
+        if spouse.gender == self.gender:
+            raise ValueError('Spouses cannot have same genders!')
+        
+        self.spouse = spouse
     
     def add_child(self, child: object) -> None:
+        if child in self.children:
+            raise Exception('Child {child} already exist!')
         self.children.append(child)
-    
-    def set_spouse(self, spouse: object) -> None:
-        self.spouse = spouse    
--- tests/unit/test_member.py
+++ tests/unit/test_member.py
@@ -1,30 +1,52 @@
+from hashlib import new
 import pytest
 
 from family_tree.member import Member, Gender
 
 #!: should we test constructor 
+#! should we test private variables
+#! dunder methods shoudl be tested
+#! shoudl we test the python types
+#! multiple assertion in one test
+
 
 @pytest.fixture(scope='class')
 def member():
-    member = Member(id=1, name='father', gender=Gender.MALE, father=None, mother=None) 
+    member = Member(name='father', gender=Gender.MALE, father=None, mother=None) 
     yield member
 
 class TestMember():
     
-    def test_add_child(self, member):
-        child = Member(id=2, name='child_1', gender=Gender.MALE, father=member, mother=None) #TODO: check
-        member.add_child(child)
+    def test_init_invalid_father_gender(self) -> None:
+        father = Member(name='father', gender=Gender.FEMALE, father=None, mother=None)
+        with pytest.raises(ValueError):
+            new_member = Member(name='member', gender=Gender.FEMALE, father=father, mother=None)
+            
+    def test_init_invalid_mother_gender(self) -> None:
+        mother = Member(name='mother', gender=Gender.MALE, father=None, mother=None)
+        with pytest.raises(ValueError):
+            new_member = Member(name='member', gender=Gender.FEMALE, father=None, mother=mother)
         
-        assert child in member.children
-    
-    def test_added_child_is_unique(self, member): #TODO:check
-        child = Member(id=3, name='child_1', gender=Gender.MALE, father=member, mother=None) #TODO: check
-        member.add_child(child)
+    def test_set_spouse(self) -> None:
+        male_member = Member(name='member1', gender=Gender.MALE, father=None, mother=None)
+        female_member = Member(name='member2', gender=Gender.FEMALE, father=None, mother=None)
         
-        number_of_same_children = [child for _child in member.children if _child == child]         
-        assert len(number_of_same_children) == 1
-    
+        male_member.set_spouse(female_member)
+        assert male_member.spouse == female_member
         
-    def test_set_spouse(self):
-        pass
+    def test_set_spouse_same_gender(self) -> None:
+        male_member_1 = Member(name='member1', gender=Gender.MALE, father=None, mother=None)
+        male_member_2 = Member(name='member2', gender=Gender.MALE, father=None, mother=None)
         
+        with pytest.raises(ValueError):
+            male_member_1.set_spouse(male_member_2)
+            
+    def test_add_child(self) -> None:
+        child = Member(name='child', gender=Gender.MALE, father=None, mother=None)
+        father = Member(name='father', gender=Gender.MALE, father=None, mother=None)
+        
+        father.add_child(child)
+        assert child in father.children
+
+        with pytest.raises(Exception):
+            father.add_child(child)        
